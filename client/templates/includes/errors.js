f45d93b1840765e230e4448685a7b1a04ed7b1d4
Roel Castaño <roelcastanomoreno@gmail.com>
2014-12-29 00:18:37 -0600

    validate post contents when editing

--- client/templates/includes/errors.js
+++ client/templates/includes/errors.js
@@ -3,3 +3,11 @@ Template.errors.helpers({
         return Errors.find();
     }
 });
+
+Template.errors.rendered =  function() {
+    var error = this.data;
+
+    Meteor.setTimeout(function () {
+        Errors.remove(error._id);
+    }, 3000);
+};
